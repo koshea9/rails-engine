e521924c63d6536298584a274465870a185e78db
koshea9 <keegan.oshea9@gmail.com>
2021-05-21 21:44:52 -0600

    add serializer

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -9,15 +9,15 @@ RSpec.describe "Merchants API endpoint" do
     expect(response).to be_successful
 
     merchants = JSON.parse(response.body, symbolize_names: true)
+ 
+    expect(merchants[:data].count).to eq(20)
 
-    expect(merchants.count).to eq(20)
-
-    merchants.each do |merchant|
+    merchants[:data].each do |merchant|
       expect(merchant).to have_key(:id)
-      expect(merchant[:id]).to be_an(Integer)
+      expect(merchant[:id]).to be_an(String)
 
-      expect(merchant).to have_key(:name)
-      expect(merchant[:name]).to be_an(String)
+      expect(merchant[:attributes]).to have_key(:name)
+      expect(merchant[:attributes][:name]).to be_an(String)
     end
   end
 end
