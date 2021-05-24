e18edc48b45b8797906a86505798f2cce9263aba
koshea9 <keegan.oshea9@gmail.com>
2021-05-24 17:25:14 -0600

    change database to match enums for transactions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_24_230516) do
+ActiveRecord::Schema.define(version: 2021_05_24_232140) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,9 +63,9 @@ ActiveRecord::Schema.define(version: 2021_05_24_230516) do
     t.bigint "invoice_id"
     t.string "credit_card_number"
     t.string "credit_card_expiration_date"
-    t.string "result"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "result"
     t.index ["invoice_id"], name: "index_transactions_on_invoice_id"
   end
 
