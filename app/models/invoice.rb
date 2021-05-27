7a205b12c2baa435a78ca2047f0ab41ab88cd711
koshea9 <keegan.oshea9@gmail.com>
2021-05-26 18:42:26 -0600

    add method and test for invoice revenue not shipped

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -8,4 +8,13 @@ class Invoice < ApplicationRecord
   has_many :items, through: :invoice_items
 
   enum status: { 'shipped' => 0, 'returned' => 1, 'packaged' => 2 }
+
+  def self.unshipped_revenue_by_invoice(invoice_limit)
+    joins(:invoice_items)
+    .where.not(status: "shipped")
+    .select("invoices.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS potential_revenue")
+    .group(:id)
+    .order("potential_revenue DESC")
+    .limit(invoice_limit)
+  end
 end
--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -12,4 +12,35 @@ RSpec.describe Invoice, type: :model do
   describe 'validations' do
     it { should validate_presence_of :status }
   end
+
+  describe 'class methods' do
+    before :each do
+      FactoryBot.reload
+  end
+
+  describe '::unshipped_revenue_by_invoice' do
+    it 'returns the total revenue for a user selected quantity of invoices' do
+      invoice_1 = create(:shipped_invoice)
+      invoice_2 = create(:shipped_invoice)
+      invoice_3 = create(:not_shipped_invoice)
+      invoice_4 = create(:shipped_invoice)
+      invoice_5 = create(:not_shipped_invoice)
+      invoice_6 = create(:shipped_invoice)
+      invoice_7 = create(:not_shipped_invoice)
+
+      invoice_item_1 = create(:invoice_item, invoice_id: invoice_1.id, quantity: 1, unit_price: 10)
+      invoice_item_2 = create(:invoice_item, invoice_id: invoice_2.id, quantity: 5, unit_price: 10)
+      invoice_item_3 = create(:invoice_item, invoice_id: invoice_3.id, quantity: 6, unit_price: 10)
+      invoice_item_4 = create(:invoice_item, invoice_id: invoice_4.id, quantity: 2, unit_price: 10)
+      invoice_item_5 = create(:invoice_item, invoice_id: invoice_5.id, quantity: 5, unit_price: 10)
+      invoice_item_6 = create(:invoice_item, invoice_id: invoice_6.id, quantity: 10, unit_price: 10)
+      invoice_item_7 = create(:invoice_item, invoice_id: invoice_7.id, quantity: 10, unit_price: 10)
+
+      expect(Invoice.unshipped_revenue_by_invoice(3)).to eq([invoice_7, invoice_3, invoice_5])
+      expect(Invoice.unshipped_revenue_by_invoice(3).first.potential_revenue).to eq(100)
+      expect(Invoice.unshipped_revenue_by_invoice(3).second.potential_revenue).to eq(60)
+      expect(Invoice.unshipped_revenue_by_invoice(3).third.potential_revenue).to eq(50)
+      end
+    end
+  end
 end
