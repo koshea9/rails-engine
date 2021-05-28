9c820df185c5384d3029cae8cd47822fa453d4b6
koshea9 <keegan.oshea9@gmail.com>
2021-05-27 21:18:21 -0600

    start refactor and work on rest once MVP complete

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -1,19 +1,21 @@
 class Api::V1::Items::SearchController < ApplicationController
   def show
+    render json: ItemSerializer.new(evaluate_params)
+  end
+  
+  private
+
+  def evaluate_params
     if params[:name]
-      item = Item.search(params[:name]).first
-      render json: ItemSerializer.new(item)
+      return item = Item.search(params[:name]).first
     elsif params[:min_price] && params[:max_price]
-      item = Item.find_by_price_range(params[:min_price], params[:max_price]).first
-      render json: ItemSerializer.new(item)
+      return item = Item.find_by_price_range(params[:min_price], params[:max_price]).first
     elsif params[:min_price]
-      item = Item.find_by_min_price(params[:min_price]).first
-      render json: ItemSerializer.new(item)
+      return item = Item.find_by_min_price(params[:min_price]).first
     elsif params[:max_price]
-      item = Item.find_by_max_price(params[:max_price]).first
-      render json: ItemSerializer.new(item)
+      return item = Item.find_by_max_price(params[:max_price]).first
     else
-      render json: {status: "error", code: 400}
+      return item = nil
     end
   end
 end
